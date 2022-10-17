c16e76b8d584c4d60a99788ce099eb51518bdf4c
Eman-A44 <imanalnoor2019@gmail.com>
2022-10-17 09:34:25 -0700

    Alphabit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * Description: program that prints the alphabet in lowercase, followed by a new line
- *   * You can only use the putchar
- *    * Return: 0
+ *  * Description: prints alphabet in lowercase,
+ *   * followed by a new line
+ *    * You can only use the putchar
+ *     * Return: 0
       */
 
 int main(void)

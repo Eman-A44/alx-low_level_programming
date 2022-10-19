bbe7c0cbce025fc30addded2aefe26daa193ee16
Eman-A44 <imanalnoor2019@gmail.com>
2022-10-19 04:32:52 -0700

    .c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  * print_sign - function that prints the sign of a number..
+ *   * @n: The input number to check
+ *    * Return: int
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+	x = x * (-1);
+	return (x);
+}
+else
+{
+	return (x);
+}
+}
